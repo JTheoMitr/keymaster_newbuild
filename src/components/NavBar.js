c65fed7b67d5e2e5083f465c10664466235838bf
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-06-29 00:54:37 -0400

    added basic styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,49 +3,55 @@ import { NavLink } from 'react-router-dom';
 
 const link = {
   width: '100px',
-  padding: '12px',
+  padding: '5px',
   margin: '0 6px 6px',
-  background: 'blue',
   textDecoration: 'none',
   color: 'white',
-  textAlign: 'center'
 }
 
 const NavBar = () => {
   return (
+
     <div className='navbar'>
+        <div className='navButton'>
       <NavLink
         to="/scales"
         exact
         style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
       >Home</NavLink>
+      </div>
+
+      <br></br>
+
+    <div className='navButton'>
       <NavLink
         to="/about"
         exact
         style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
       >About</NavLink>
+      </div>
+
+        <br></br>
+
+    <div className='navButton'>
       <NavLink
         to="/contact"
         exact
         style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
       >Contact</NavLink>
+      </div>
+
+      <br></br>
+
+    <div className='navButton'>
       <NavLink
         to="/scales/new"
         exact
         style={link}
-        activeStyle={{
-          background: 'darkblue'
-        }}
       >New</NavLink>
+      </div>
+
+
     </div>
   );
 };
