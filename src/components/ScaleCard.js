bdad282f3c861e7e9332bcf5673388efc80adbbd
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-10-24 22:57:32 -0500

    added example link logic to ScaleCard

--- src/components/ScaleCard.js
+++ src/components/ScaleCard.js
@@ -15,15 +15,27 @@ const ScaleCard = ({allScales, name, id, description, notes}) => {
             <p>{description}</p>
             <h3>{notes}</h3>
 
-            {
+            { // check if relative scales exist
                 (relatives.length > 0)
                 ? <p>Relative Scales: {relatives.map(rel => <Link key={rel.id} to={`/scales/${rel.id}`}>{rel.name}</Link>)}</p> 
                 : <p></p>
             }
 
-            {
+            { // check if parallel scales exist
                 (parallels.length > 0)
                 ? <p>Parallel Scales: {parallels.map(par => par.name)}</p>
+                : <p></p>
+            }
+
+            { // Confirm scale type and display link to examples
+                (name.includes('Harmonic Major'))
+                ? <p><a href='https://musictales.club/article/9-beatles-songs-combine-harmonic-major-ionian-mode' target="_blank" rel="noreferrer">Songs in Harmonic Major</a></p>
+                : (name.includes('Harmonic Minor'))
+                ? <p><a href='https://www.youtube.com/watch?v=-amnre-0I3w' target="_blank" rel="noreferrer">Songs in Harmonic Minor</a></p>
+                : (name.includes('Melodic Minor'))
+                ? <p><a href='https://www.youtube.com/watch?v=-8CLp1uSFoA' target="_blank" rel="noreferrer">Songs in Melodic Minor</a></p>
+                : (name.includes('Major') && !(name.includes('Harmonic')))
+                ? <p><a href='https://www.youtube.com/playlist?list=PLBC7CA504C2931E3E' target="_blank" rel="noreferrer">Songs in the Major key</a></p>
                 : <p></p>
             }
             
