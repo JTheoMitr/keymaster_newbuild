3246d29987d85f5c9db9b002f42a46d32ace9282
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-12-28 23:08:46 -0600

    first commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,65 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ScaleCard from './ScaleCard'
 
-class SearchBar extends React.Component {
+const SearchBar = (props) => {
 
-    state = {
-        name: ""
-    }
+    const [name, setName] = useState("");
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    
-
-    searchScales = () => {
-
-        console.log(this.state.name.toUpperCase().split('-'))
+    const onChange = (event) => {
+        setName(event.target.value);
+      };
          
-            const searchedScales = 
+        const searchedScales = 
 
             // check for value(s) in search params, otherwise display all scales
             
-            this.props.allScales.filter(scale => 
+            props.allScales.filter(scale => 
 
-                this.state.name.length > 0 ?
+                name.length > 0 ?
 
-                    this.state.name.toUpperCase().split('-').every(elem =>
+                    name.toUpperCase().split('-').every(elem =>
                         
                         (scale.notes.toUpperCase().split('-').includes(elem)) || elem === "" ) :
 
                     !!scale.name)
 
 
-            return searchedScales.map(scale => 
+            return(
 
-            
-                <div key={scale.id * 100} >
-                <ScaleCard 
-                id={scale.id} 
-                key={scale.id} 
-                name={scale.name} 
-                notes={scale.notes} 
-                description={scale.description} 
-                allScales={this.props.allScales}
+                <div>
+                    <input onChange={onChange} type="text" name="name" placeholder="Search Notes in Dash Format (A-B-C#-D etc)" value={name}></input>
 
-                />
-                <div key={scale.id * 10} className="parallax"></div>
-                </div>)
-    }
+                    {searchedScales.map(scale => 
 
-    render() {
-        return(
-            <div>
-                <input onChange={this.handleChange} type="text" name="name" placeholder="Search Notes in Dash Format (A-B-C#-D etc)" value={this.state.name}></input>
-                {this.searchScales()}
-                
-            </div>
-        )
-    }
+                        <div key={scale.id * 100} >
+
+                            <ScaleCard 
+                                id={scale.id} 
+                                key={scale.id} 
+                                name={scale.name} 
+                                notes={scale.notes} 
+                                description={scale.description} 
+                                allScales={props.allScales}
+                            />
+                            
+                            <div key={scale.id * 10} className="parallax"></div>
+                        
+                        </div>)}
+
+                </div>
+            )
 
 }
 
